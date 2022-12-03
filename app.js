c58c2ee80619d8c446393d514b5acd2a12f89d66
akashar13 <77505212+akashar13@users.noreply.github.com>
2022-12-03 13:44:29 +0530

    APP

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.removeHeader('x-powered-by');
     //set the allowed HTTP methods to be requested
-    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
     //headers clients can use in their requests
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     //allow request to continue and be handled by routes
