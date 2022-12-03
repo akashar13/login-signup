5a85b468ed2563e526188be223b5f13a5119c0e0
akashar13 <77505212+akashar13@users.noreply.github.com>
2022-12-03 10:30:34 +0530

    corse

--- app.js
+++ app.js
@@ -18,5 +18,16 @@ const app = express()
 app.use(morgan('dev'))
 const userRouter = require('./routes/routes')
 app.use(express.json());
-app.use('/api/v1/user',userRouter)
+app.use('/api/v1/user', userRouter);
+app.use((req, res, next) => {
+    //allow access from every, elminate CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.removeHeader('x-powered-by');
+    //set the allowed HTTP methods to be requested
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    //headers clients can use in their requests
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    //allow request to continue and be handled by routes
+    next();
+});
 module.exports = app
