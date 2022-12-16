43ba2545a84b463351556c8ce3b7d177e016ba2a
akashar13 <77505212+akashar13@users.noreply.github.com>
2022-12-16 11:39:25 +0530

    fix

--- app.js
+++ app.js
@@ -14,26 +14,61 @@
 // });
 const express = require('express')
 const morgan = require('morgan')
+
+
 const app = express()
 const globalErrorHandler = require('./controller/errorController');
 const cors = require("cors"); //Add this
 
-
+global.__basedir = __dirname;
 app.use(morgan('dev'))
 const userRouter = require('./routes/routes')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use('/api/v1/user', userRouter);
-app.use((res, req, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE');
+var corsOptions = {
 
-    next();
-});
-app.use(cors()); //And add this line as well
+    origin: "*",
+
+    allowedHeaders: [
+
+        "Content-Type",
+
+        "Authorization",
+
+        "Accept",
+
+        "x-reset-token",
+
+        "x-invite-token",
+
+        "x-api-key",
+
+        "x-www-form-urlencoded",
+
+    ],
+
+    credentials: true,
+
+};
+var fs  = require("fs");
+
+app.use(cors(corsOptions));
+app.use(express.static("assets"));
+
+app.use("/images", express.static("images"));
+
+
+// app.use((res, req, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     );
+//     res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE');
+
+//     next();
+// });
+// app.use(cors()); //And add this line as well
 app.use(globalErrorHandler);
 module.exports = app
